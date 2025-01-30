37f35a6b771f4d94bc87dfd9c9028ff7df71da9d
jeferson-deve <mp_bra_nco@hotmail.com>
2025-01-30 20:16:49 -0300

    Criando a janela de cadastro de ora

--- Form_home.Designer.cs
+++ Form_home.Designer.cs
@@ -28,18 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            panel_central = new Panel();
+            button5 = new Button();
+            button6 = new Button();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 558);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1058, 30);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(179, 558);
+            panel2.TabIndex = 1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(3, 256);
+            button4.Name = "button4";
+            button4.Size = new Size(170, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Apontamento";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 227);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Ordem de produção";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 198);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Items da obra";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Cadastro de Obra";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(170, 148);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel_central
+            // 
+            panel_central.Dock = DockStyle.Fill;
+            panel_central.Location = new Point(179, 0);
+            panel_central.Name = "panel_central";
+            panel_central.Size = new Size(879, 558);
+            panel_central.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 285);
+            button5.Name = "button5";
+            button5.Size = new Size(170, 23);
+            button5.TabIndex = 5;
+            button5.Text = "Histório Obras";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 314);
+            button6.Name = "button6";
+            button6.Size = new Size(170, 23);
+            button6.TabIndex = 6;
+            button6.Text = "Configuração";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form_home
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(1058, 588);
+            Controls.Add(panel_central);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Form_home";
             Text = "PRIME";
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel_central;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button6;
+        private Button button5;
     }
 }
--- Form_item_obra.Designer.cs
+++ Form_item_obra.Designer.cs
@@ -28,12 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_item_obra";
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox3 = new GroupBox();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(8, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(934, 133);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Area de Cadastro";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(417, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Nome da Obra";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(358, 13);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(152, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(271, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Status";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(97, 13);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(126, 23);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Data Cadastro";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(8, 151);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(934, 295);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Lista de Obras";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(587, 40);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(341, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(520, 48);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 6;
+            label4.Text = "ID - Obra";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(97, 71);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(417, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 77);
+            label5.Name = "label5";
+            label5.Size = new Size(44, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Cliente";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(587, 71);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(341, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(520, 77);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 15);
+            label6.TabIndex = 10;
+            label6.Text = "Categoria:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(97, 100);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(81, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 108);
+            label7.Name = "label7";
+            label7.Size = new Size(72, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Quantidade:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(587, 19);
+            label8.Name = "label8";
+            label8.Size = new Size(256, 15);
+            label8.TabIndex = 14;
+            label8.Text = "id projeto ---------------------------------------";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(16, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(103, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Deletar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Dock = DockStyle.Bottom;
+            groupBox3.Location = new Point(0, 452);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(954, 44);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Linha de Botões";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(194, 15);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 16;
+            button3.Text = "Cancelar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 19);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(928, 273);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(856, 104);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 17;
+            button4.Text = "Filtrar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form_item_obra
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(954, 496);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Form_item_obra";
+            Text = "Form_item_obra";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private Button button2;
+        private Button button1;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label6;
+        private GroupBox groupBox3;
+        private Button button3;
+        private DataGridView dataGridView1;
+        private Button button4;
     }
 }
