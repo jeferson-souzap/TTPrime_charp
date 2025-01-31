6b2ebc546e283db9f38d566a4c956c8fb698125f
jeferson-deve <mp_bra_nco@hotmail.com>
2025-01-31 00:01:29 -0300

    Janela de ordem de produção

--- Form_ordem_prod.Designer.cs
+++ Form_ordem_prod.Designer.cs
@@ -28,12 +28,380 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_ordem_prod";
+            groupBox3 = new GroupBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label4 = new Label();
+            comboBox2 = new ComboBox();
+            label5 = new Label();
+            comboBox3 = new ComboBox();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            groupBox4 = new GroupBox();
+            dateTimePicker2 = new DateTimePicker();
+            label10 = new Label();
+            richTextBox1 = new RichTextBox();
+            label11 = new Label();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Dock = DockStyle.Bottom;
+            groupBox3.Location = new Point(0, 452);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(954, 44);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Linha de Botões";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(194, 15);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 16;
+            button3.Text = "Cancelar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(103, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Deletar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(16, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Criar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(12, 199);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(930, 247);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Lista de OP";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 19);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(924, 225);
+            dataGridView1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(523, 181);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = ".";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(23, 15);
+            label1.TabIndex = 0;
+            label1.Text = "OP";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlLight;
+            textBox1.Location = new Point(108, 16);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(167, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(345, 16);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(166, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(281, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Status OP";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 109);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Data Abertura";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(109, 103);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(180, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 48);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 7;
+            label4.Text = "ID - Obra";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(109, 45);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(402, 23);
+            comboBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 77);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Estapas / Setores";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(108, 74);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(220, 23);
+            comboBox3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Qtd. Planejada";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(96, 16);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(103, 23);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ControlLight;
+            textBox3.Location = new Point(298, 16);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(93, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(205, 19);
+            label7.Name = "label7";
+            label7.Size = new Size(86, 15);
+            label7.TabIndex = 13;
+            label7.Text = "Qtd. Produzida";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ControlLight;
+            textBox4.Location = new Point(298, 45);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(93, 23);
+            textBox4.TabIndex = 18;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(205, 48);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 15);
+            label8.TabIndex = 17;
+            label8.Text = "Peso Produzido";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(96, 45);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(103, 23);
+            textBox5.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(5, 48);
+            label9.Name = "label9";
+            label9.Size = new Size(87, 15);
+            label9.TabIndex = 15;
+            label9.Text = "Peso Planejado";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(label11);
+            groupBox4.Controls.Add(richTextBox1);
+            groupBox4.Controls.Add(textBox5);
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Controls.Add(label9);
+            groupBox4.Controls.Add(label7);
+            groupBox4.Controls.Add(textBox2);
+            groupBox4.Controls.Add(label8);
+            groupBox4.Controls.Add(label6);
+            groupBox4.Controls.Add(textBox3);
+            groupBox4.Location = new Point(541, 12);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(401, 181);
+            groupBox4.TabIndex = 8;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Valores";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(109, 132);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(180, 23);
+            dateTimePicker2.TabIndex = 11;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 138);
+            label10.Name = "label10";
+            label10.Size = new Size(100, 15);
+            label10.TabIndex = 10;
+            label10.Text = "Data Fechamento";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 94);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(385, 81);
+            richTextBox1.TabIndex = 19;
+            richTextBox1.Text = "";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(6, 74);
+            label11.Name = "label11";
+            label11.Size = new Size(69, 15);
+            label11.TabIndex = 20;
+            label11.Text = "Observação";
+            // 
+            // Form_ordem_prod
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(954, 496);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox3);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Form_ordem_prod";
+            Text = "Ordem de produção";
+            groupBox3.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox3;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox2;
+        private Label label5;
+        private ComboBox comboBox3;
+        private TextBox textBox2;
+        private Label label6;
+        private TextBox textBox3;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label9;
+        private GroupBox groupBox4;
+        private DateTimePicker dateTimePicker2;
+        private Label label10;
+        private Label label11;
+        private RichTextBox richTextBox1;
     }
 }
